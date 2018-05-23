c66d9abbe3355315497201fd54b2a3d831e6a942
PrabaBa <prabahridayami97@gmail.com>
2018-05-23 15:03:39 +0800

    dashbor

--- application/views/pages/dashbor.php
+++ application/views/pages/dashbor.php
@@ -124,10 +124,10 @@
         
         </div>
     </section>
-    <section id="laundry">
+    <section id="laundry" style="background-image: url('assets/image/06.jpg'); height:600px;" >
             <div class="container">
                 <p>PUT LAUNDRY HERE</p>
-                <a style="margin-top:150px; margin-bottom:50px; width:30%" href="<?php echo base_url()?>mydashbor/order" class="btn btn-primary">Order Here</a>	
+                <a style="margin-top:100px; margin-bottom:50px;width:30%" href="<?php echo base_url()?>mydashbor/order" class="btn btn-primary">Order Here</a>	
                 <?php
                     if(isset($_GET['st'])){
                         if($_GET['st']=="success"){
@@ -148,27 +148,148 @@
             </div>
         </div>
     </section>
-    <section id="promotion">
-        <div id="slide" >
-            <div class="slideshow-container" >
-                <div class="mySlides fade"> <img class="promo" src="<?php echo base_url()?>assets/image/promo1.png"> </div>
-                <div class="mySlides fade"> <img class="promo" src="<?php echo base_url()?>assets/image/promo2.png" style="width:100%"> </div>
-                <div class="mySlides fade"> <img class="promo" src="<?php echo base_url()?>assets/image/03.jpg"> </div>
-                <div class="mySlides fade"> <img class="promo" src="<?php echo base_url()?>assets/image/04.jpg" style="width:100%"> </div>
-                
-                <a class="prev" onclick="plusSlides(-1)">&#10094;</a> <a class="next" onclick="plusSlides(1)">&#10095;</a> 
-                <div style="text-align:center"> 
-                    <span class="dot" onclick="currentSlide(1)"></span> 
-                    <span class="dot" onclick="currentSlide(2)"></span> 
-                    <span class="dot" onclick="currentSlide(3)"></span>
-                    <span class="dot" onclick="currentSlide(4)"></span>
+    <section id="promotion" style="background:#fff;">
+    
+    <script src="<?php echo base_url()?>assets/js/jssor.slider-27.1.0.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        jssor_1_slider_init = function() {
+
+            var jssor_1_SlideoTransitions = [
+              [{b:-1,d:1,o:-0.7}],
+              [{b:900,d:2000,x:-379,e:{x:7}}],
+              [{b:900,d:2000,x:-379,e:{x:7}}],
+              [{b:-1,d:1,o:-1,sX:2,sY:2},{b:0,d:900,x:-171,y:-341,o:1,sX:-2,sY:-2,e:{x:3,y:3,sX:3,sY:3}},{b:900,d:1600,x:-283,o:-1,e:{x:16}}]
+            ];
+
+            var jssor_1_options = {
+              $AutoPlay: 1,
+              $SlideDuration: 800,
+              $SlideEasing: $Jease$.$OutQuint,
+              $CaptionSliderOptions: {
+                $Class: $JssorCaptionSlideo$,
+                $Transitions: jssor_1_SlideoTransitions
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*#region responsive code begin*/
+
+            var MAX_WIDTH = 3000;
+
+            function ScaleSlider() {
+                var containerElement = jssor_1_slider.$Elmt.parentNode;
+                var containerWidth = containerElement.clientWidth;
+
+                if (containerWidth) {
+
+                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                    jssor_1_slider.$ScaleWidth(expectedWidth);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            /*#endregion responsive code end*/
+        };
+    </script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,regular,italic,700,700italic&subset=latin-ext,greek-ext,cyrillic-ext,greek,vietnamese,latin,cyrillic" rel="stylesheet" type="text/css" />
+    <style>
+        /*jssor slider loading skin spin css*/
+        .jssorl-009-spin img {
+            animation-name: jssorl-009-spin;
+            animation-duration: 1.6s;
+            animation-iteration-count: infinite;
+            animation-timing-function: linear;
+        }
+
+        @keyframes jssorl-009-spin {
+            from { transform: rotate(0deg); }
+            to { transform: rotate(360deg); }
+        }
+
+        /*jssor slider bullet skin 032 css*/
+        .jssorb032 {position:absolute;}
+        .jssorb032 .i {position:absolute;cursor:pointer;}
+        .jssorb032 .i .b {fill:#fff;fill-opacity:0.7;stroke:#000;stroke-width:1200;stroke-miterlimit:10;stroke-opacity:0.25;}
+        .jssorb032 .i:hover .b {fill:#000;fill-opacity:.6;stroke:#fff;stroke-opacity:.35;}
+        .jssorb032 .iav .b {fill:#000;fill-opacity:1;stroke:#fff;stroke-opacity:.35;}
+        .jssorb032 .i.idn {opacity:.3;}
+
+        /*jssor slider arrow skin 051 css*/
+        .jssora051 {display:block;position:absolute;cursor:pointer;}
+        .jssora051 .a {fill:none;stroke:#fff;stroke-width:360;stroke-miterlimit:10;}
+        .jssora051:hover {opacity:.8;}
+        .jssora051.jssora051dn {opacity:.5;}
+        .jssora051.jssora051ds {opacity:.3;pointer-events:none;}
+    </style>
+    <div id="jssor_1" style="position:relative;margin:0 auto;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;visibility:hidden;">
+        <!-- Loading Screen -->
+        <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
+            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/spin.svg" />
+        </div>
+        <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:1300px;height:500px;overflow:hidden;">
+            <div data-p="225.00">
+                <img data-u="image" src="<?php echo base_url()?>assets/image/03.jpg" />
+                <div data-u="caption" data-t="0" style="position:absolute;top:120px;left:75px;width:470px;height:220px;">
+                    <img style="position:absolute;top:0px;left:0px;width:470px;height:220px;" src="<?php echo base_url()?>assets/img/c-phone-horizontal.png" />
+                    <div style="position:absolute;top:4px;left:45px;width:379px;height:213px;overflow:hidden;">
+                        <img data-u="caption" data-t="1" style="position:absolute;top:0px;left:0px;width:379px;height:213px;" src="<?php echo base_url()?>assets/img/c-slide-1.jpg" />
+                        <img data-u="caption" data-t="2" style="position:absolute;top:0px;left:379px;width:379px;height:213px;" src="<?php echo base_url()?>assets/img/c-slide-3.jpg" />
+                    </div>
+                    <img style="position:absolute;top:4px;left:45px;width:379px;height:213px;" src="img/c-navigator-horizontal.png" />
+                    <img data-u="caption" data-t="3" style="position:absolute;top:476px;left:454px;width:63px;height:77px;" src="<?php echo base_url()?>assets/img/hand.png" />
                 </div>
             </div>
+            <div data-p="225.00">
+                <img data-u="image" src="<?php echo base_url()?>assets/image/promo1.png" />
+            </div>
+            <div data-p="225.00">
+                <img data-u="image" src="<?php echo base_url()?>assets/image/promo2.png" />
+                <div style="position:absolute;top:30px;left:30px;width:480px;height:130px;font-family:'Roboto Condensed',sans-serif;font-size:40px;color:#000000;line-height:1.5;padding:5px 5px 5px 5px;box-sizing:border-box;">
+<br />
+                </div>
+                <div style="position:absolute;top:300px;left:30px;width:480px;height:130px;font-family:'Roboto Condensed',sans-serif;font-size:30px;color:#000000;line-height:1.27;padding:5px 5px 5px 5px;box-sizing:border-box;"></div>
+            </div>
         </div>
+        <!-- Bullet Navigator -->
+        <div data-u="navigator" class="jssorb032" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+            <div data-u="prototype" class="i" style="width:16px;height:16px;">
+                <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                    <circle class="b" cx="8000" cy="8000" r="5800"></circle>
+                </svg>
+            </div>
+        </div>
+        <!-- Arrow Navigator -->
+        <div data-u="arrowleft" class="jssora051" style="width:65px;height:65px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
+            </svg>
+        </div>
+        <div data-u="arrowright" class="jssora051" style="width:65px;height:65px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+            <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
+            </svg>
+        </div>
+    </div>
+    <script type="text/javascript">jssor_1_slider_init();</script>
     </section>
-    <section id="find-us">
+    <section id="find-us" style="background:#fff">
         <div class="container">
-            <h3 style="margin-top:-70px;">FIND US HERE</h3>
+            <h3 style="margin-top:-70px; color:#000;">FIND US HERE</h3>
             <iframe src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=-8.7982727,115.17239210000002&amp;q=Jln.%20Raya%20Kampus%20Unud%2C%20Bukit%20Jimbaran+(Ondry)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=A&amp;output=embed" width="100%" height="515px" frameborder="0" style="border:0" scrolling="no" marginheight="0" marginwidth="0"></iframe></script>
         </div>
     </section>
@@ -223,70 +344,6 @@
                     }
                 })
             })
-        </script>
-        <script>
-
-            var slideIndex = 0;
-            showSlides();
-            var slides,dots;
-
-            function plusSlides(position) {
-                slideIndex += position;
-                if (slideIndex > slides.length) {slideIndex = 1}
-                else if(slideIndex < 1){slideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-
-                }
-                    slides[slideIndex-1].style.display = "block";  
-                    dots[slideIndex-1].className += " active";
-                }
-
-            function currentSlide(index) {
-                if (index > slides.length) {index = 1}
-                else if(index < 1){index = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                    slides[index-1].style.display = "block";  
-                    dots[index-1].className += " active";
-                }
-
-            function showSlides() {
-                var i;
-                slides = document.getElementsByClassName("mySlides");
-                dots = document.getElementsByClassName("dot");
-                for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";  
-                }
-                slideIndex++;
-                if (slideIndex> slides.length) {slideIndex = 1}    
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 5000); // Change image every 3 seconds
-            }
-
-            function initMap() {
-                var uluru = {lat: -25.363, lng: 131.044};
-                var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-                });
-                var marker = new google.maps.Marker({
-                position: uluru,
-                map: map
-                });
-            }
-
         </script>
         <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFuysYvwzP87U4KqDVk423Ew8AYb7y920">
